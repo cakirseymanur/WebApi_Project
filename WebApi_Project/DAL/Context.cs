f978ca888f416b128a4f33dc811cfadd11fc6e69
cakirseymanur <seyma_akay97@hotmail.com>
2022-12-23 21:09:12 +0300

    Add project files.

---
+++ WebApi_Project/DAL/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi_Project.DAL.Entities;
+
+namespace WebApi_Project.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;initial catalog=DbWebApi;integrated security=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
